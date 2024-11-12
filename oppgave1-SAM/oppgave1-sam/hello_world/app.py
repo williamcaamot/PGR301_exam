ff906ea74dc0141a5bc2510339fceb1378406f5e
williamcaamot <william-ca@live.no>
2024-11-12 16:06:37 +0100

    Change code

--- oppgave1-SAM/oppgave1-sam/hello_world/app.py
+++ oppgave1-SAM/oppgave1-sam/hello_world/app.py
@@ -1,4 +1,7 @@
 import json
+import base64
+import boto3
+import random
 
 # import requests
 
@@ -33,6 +36,8 @@ def lambda_handler(event, context):
 
     #     raise e
 
+    generateAndSaveImage()
+
     return {
         "statusCode": 200,
         "body": json.dumps({
@@ -40,3 +45,44 @@ def lambda_handler(event, context):
             # "location": ip.text.replace("\n", "")
         }),
     }
+
+
+def generateAndSaveImage():
+    # Set up the AWS clients
+
+    bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
+    s3_client = boto3.client("s3")
+
+    # Define the model ID and S3 bucket name (replace with your actual bucket name)
+    model_id = "amazon.titan-image-generator-v1"
+    bucket_name = "pgr301-couch-explorers"
+
+    # Frank; Important; Change this prompt to something else before the presentation with the investors!
+    prompt = "Investors, with circus hats, giving money to developers with large smiles"
+
+    seed = random.randint(0, 2147483647)
+    s3_image_path = f"47/titan_{seed}.png"
+
+    native_request = {
+        "taskType": "TEXT_IMAGE",
+        "textToImageParams": {"text": prompt},
+        "imageGenerationConfig": {
+            "numberOfImages": 1,
+            "quality": "standard",
+            "cfgScale": 8.0,
+            "height": 1024,
+            "width": 1024,
+            "seed": seed,
+        }
+    }
+
+    response = bedrock_client.invoke_model(modelId=model_id, body=json.dumps(native_request))
+    model_response = json.loads(response["body"].read())
+
+    # Extract and decode the Base64 image data
+    base64_image_data = model_response["images"][0]
+    image_data = base64.b64decode(base64_image_data)
+
+    # Upload the decoded image data to S3
+    s3_client.put_object(Bucket=bucket_name, Key=s3_image_path, Body=image_data)
+    print("Object successfully saved")
