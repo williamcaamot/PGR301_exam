f6a6fb76f61034c035f43d7dd85bd44a9df51d3c
williamcaamot <william-ca@live.no>
2024-11-14 18:36:57 +0100

    Add cloudwatch metric and alarm with SNS to send alarm to email

---
+++ oppgave2_infra/terraform/oppgave4_metrics.tf
@@ -0,0 +1,37 @@
+# SQS Queue
+resource "aws_sqs_queue" "image_queue" {
+  name = var.sqs_queue_name
+}
+
+# CloudWatch Alarm for ApproximateAgeOfOldestMessage
+resource "aws_cloudwatch_metric_alarm" "sqs_approximate_age_of_oldest_message" {
+  alarm_name          = "${var.sqs_queue_name}-ApproximateAgeOfOldestMessage"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 1
+  metric_name         = "ApproximateAgeOfOldestMessage"
+  namespace           = "AWS/SQS"
+  period              = var.sqs_alarm_period
+  statistic           = "Maximum"
+  threshold           = var.sqs_alarm_threshold
+  alarm_description   = "Alarm when the age of the oldest message in the SQS queue exceeds ${var.sqs_alarm_threshold} seconds."
+
+  dimensions = {
+    QueueName = aws_sqs_queue.image_queue.name
+  }
+
+  alarm_actions = [
+    aws_sns_topic.sqs_alarms.arn
+  ]
+}
+
+# SNS Topic for CloudWatch Alarms
+resource "aws_sns_topic" "sqs_alarms" {
+  name = "${var.sqs_queue_name}-sqs-alarms"
+}
+
+# SNS Subscription (e.g., Email)
+resource "aws_sns_topic_subscription" "sqs_alarms_subscription" {
+  topic_arn = aws_sns_topic.sqs_alarms.arn
+  protocol  = "email"
+  endpoint  = var.sqs_email_for_alarms
+}
--- oppgave2_infra/terraform/variables.tf
+++ oppgave2_infra/terraform/variables.tf
@@ -22,3 +22,22 @@ variable "kandidatnummer" {
     type        = string
     default     = "47"
 }
+
+
+variable "sqs_alarm_threshold" {
+  description = "Threshold for the ApproximateAgeOfOldestMessage metric in seconds."
+  type        = number
+  default     = 30  # Default to 10 minutes
+}
+
+variable "sqs_alarm_period" {
+  description = "The period in seconds over which the metric is evaluated."
+  type        = number
+  default     = 60  # Default to 5 minutes
+}
+
+variable "sqs_email_for_alarms" {
+  description = "The period in seconds over which the metric is evaluated."
+  type        = string
+  default     = "william-ca@live.no"
+}
