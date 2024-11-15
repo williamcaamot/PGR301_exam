de0c60b5ac8beee55ca058f1a7f9b3bbce24878b
williamcaamot <william-ca@live.no>
2024-11-15 15:16:18 +0100

    Modularize Lambda function

--- oppgave2_infra/terraform/outputs.tf
+++ oppgave2_infra/terraform/outputs.tf
@@ -1,8 +1,3 @@
-output "lambda_function_url" {
-  description = "URL for invoking the Lambda function"
-  value       = aws_lambda_function.image_generation_lambda.invoke_arn
-}
-
 output "sqs_queue_url" {
   description = "URL of the SQS queue"
   value       = aws_sqs_queue.image_queue.id
